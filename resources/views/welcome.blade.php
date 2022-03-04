cec56c6a935ce5075920c0e78cb0b7cd78f0f949
Matteo Pompei <datxizzan@gmail.com>
2022-03-04 16:56:46 +0100

    Bonus fatto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>laravel-model-controller</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-  @dd($appoggio)
+  <div class="container">
+    @foreach ($appoggio as $item)
+      <div class="card">
+        <span><strong>Titolo:</strong> {{ $item->title }}</span>
+        <span><strong>Titolo originale:</strong> {{ $item->original_title }}</span>
+        <span><strong>Nazionalità:</strong> {{ $item->nationality }}</span>
+        <span><strong>Data d'uscita:</strong> {{ $item->date }}</span>
+        <span><strong>Voto:</strong> {{ $item->vote }}</span>
+      </div>
+    @endforeach
+  </div>
 </body>
 
 </html>
